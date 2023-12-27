e9a9a21dbe5950350e2d91b1e306fdbcd60c2d99
samuel21695 <samuel21695@gmail.com>
2023-12-27 21:07:11 +0900

    ✨ function appendString

--- basic.js
+++ basic.js
@@ -5,3 +5,18 @@ function splitString(stringParams) {
   let result = stringParams.split(' ');
   return result;
 }
+
+function appendString(stringArray, callback) {
+  let body = "";
+  // body는 단순 지역변수이며, 콜백함수에서 '전달'받아서 사용합니다.
+  // 이것을 인자를 전달한다고 표현합니다.
+  // 여기서 지역변수 body는 클로저가 아닙니다.
+  // 호출되고나서부터는 메모리 상에서 사라집니다.
+  // 눈에 보이지 않는 개념으로, 다분히 추상적인 개념입니다.
+  stringArray.forEach((element) => {
+    body = body + element;
+    // 위보다는 아래가 보다 깔끔한 처리입니다.
+    // body += element;
+  });
+  callback(body);
+};
