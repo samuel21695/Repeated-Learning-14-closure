c68b70c05e0469856fde139a9b82f352b711b74e
samuel21695 <samuel21695@gmail.com>
2023-12-27 21:14:28 +0900

    ✨ function createStringAppender

---
+++ callback-closure.js
@@ -0,0 +1,12 @@
+const stringExample = "참깨빵 위에 순쇠고기 패티 두 장 특별한 소스 양상추"
+
+function createStringAppender() {
+  let body = ""; // 클로저에 의해 "기억"될 변수
+  // 리턴이 함수로 처리되어, body를 업데이트 할 수 있으며, React에서 상당히 많이 사용되는 패턴입니다.
+  return function(stringArray, callback) {
+    stringArray.forEach((element) => {
+      body += element; //클로저를 사용하여 body 변수를 업데이트 
+    });
+    callback(body);
+  };
+}
